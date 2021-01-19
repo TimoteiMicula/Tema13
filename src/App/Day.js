745e8b42db2a7870697318d7f203168160f919c9
Timotei Micula <timotei.micula@emaneul.ro>
2021-01-19 17:32:02 +0200

    add2

--- src/App/Day.js
+++ src/App/Day.js
@@ -6,6 +6,7 @@ const Day = ({ day, list }) => {
 	return (
 		<div>
 			<div>
+			<br/>	<br/>
 				<div><strong>{format(new Date(day), 'eeee, dd LLLL')}</strong></div>
 
 				{list.map((item) => {
@@ -14,10 +15,13 @@ const Day = ({ day, list }) => {
 							<br/>
 							<div >{item?.dt_txt}</div>
 							<br />																			
-							<div> {item?.main.temp} C</div>
+							<div class="temp"> Temperatura: {item?.main.temp } C</div>
+							<div class="temp"> Temperatura resimțită: {item?.main.feels_like} C</div>
+							<div class="temp"> Viteza vantului: {item?.wind.speed} km/h</div>
+							<div class="temp" key={item?.weather.description}>Descriere: {item?.weather[0].description} </div>
+
 								
 						
-							<hr />
 					</div>
 					)
 				})}
